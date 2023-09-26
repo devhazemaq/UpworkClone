1af8f6a842c07c6dc26e4ca4eb3e6dee15066523
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:03:53 +0300

    Page profile

---
+++ src/app/(user)/profile/page.tsx
@@ -0,0 +1,14 @@
+import ProfilePage from "@/components/pages/ProfilePage";
+
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "profile",
+  
+};
+
+
+const profile = () => < ProfilePage />;
+
+export default profile;
