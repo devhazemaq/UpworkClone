11f68a00c24583dd8f66f74cc2a498855d658cd5
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:02:08 +0300

    Main Page

---
+++ src/app/(guest)/page.tsx
@@ -0,0 +1,19 @@
+import SignupPage from "@/components/pages/SignupPage";
+import Login from "./login/page";
+import HomePage from "@/components/pages/HomePage";
+import JopInformationPage from "@/components/pages/JopInformationPage";
+import ProfilePage from "@/components/pages/ProfilePage";
+
+
+
+
+const page = () => <HomePage/>
+
+{/* <ProfilePage/> */}
+
+
+{/* <JopInformationPage/> */}
+{/* <HomePage /> */}
+
+
+export default page;
