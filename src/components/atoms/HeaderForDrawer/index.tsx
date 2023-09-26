b0c0a030e1aa65fa4e40a19eea06cbe78f02b767
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:15:22 +0300

    Header for drawer

---
+++ src/components/atoms/HeaderForDrawer/index.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from 'react'
+import { StyleMainHeaderForDrawer } from './style'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Box, IconButton } from '@mui/material';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import Link from 'next/link';
+
+
+const HeaderForDrawer = () => {
+  return (
+    <StyleMainHeaderForDrawer>
+      <Box className="box__main" >
+        <IconButton aria-label="ArrowBackIosIcon" >
+
+          <ArrowBackIosIcon fontSize='medium' />
+
+        </IconButton>
+        <Box className="box__new__window">
+          <OpenInNewIcon  fontSize='small'/>  <Link href={"/job"}>Open job in a new window</Link>
+        </Box>
+      </Box>
+    </StyleMainHeaderForDrawer>
+  )
+}
+
+export default HeaderForDrawer
---
+++ src/components/atoms/HeaderForDrawer/style.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+
+export const StyleMainHeaderForDrawer = styled.div`
+  /* -s--- box main ---  */
+  .box__main {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 30px;
+
+    .css-i4bv87-MuiSvgIcon-root {
+      position: relative;
+      left: 5px;
+    }
+
+    .box__new__window {
+      width: fit-content;
+      display: flex;
+
+      color: ${theme.colors.colorGreenYellow};
+      a:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+  /* -s--- box main ---  */
+`;
