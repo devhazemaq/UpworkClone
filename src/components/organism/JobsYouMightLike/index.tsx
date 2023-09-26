08c23cbe806d26348364d0d7b7254096692c072c
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:53:51 +0300

    Basic Tabs to show jops and saved jops

---
+++ src/components/organism/JobsYouMightLike/index.tsx
@@ -0,0 +1,87 @@
+
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CardJob from '../CardJobs';
+import { SmallText2 } from '@/components/atoms/typography';
+import { StyleMainJobsYouMightLike } from './style';
+import { API_URL_ALLJOPS } from '@/config/api';
+import CardJobs from '../CardJobs';
+import CardsJobsSaved from '../CardsJobsSaved/CardsJobsSaved';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+          {value === 0 && <CardJobs  />}
+        </Box>
+      )}
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+          {value === 1 && <CardsJobsSaved  />}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+
+  return (
+    <StyleMainJobsYouMightLike>
+
+
+      <Box sx={{ width: '100%' ,border: '1px solid #D5E0D5' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Best Matches" {...a11yProps(0)} />
+            <Tab label="Saved Jobs" {...a11yProps(1)} />
+            {/* <Tab label="Item Three" {...a11yProps(2)} /> */}
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <SmallText2 className="title__tap">Browse jobs that match your experience to a client&apos;s hiring preferences. Ordered by most relevant</SmallText2>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          
+        </CustomTabPanel>
+        {/* <CustomTabPanel value={value} index={2}>
+          Item Three
+        </CustomTabPanel> */}
+      </Box>
+    </StyleMainJobsYouMightLike>
+
+  );
+}
---
+++ src/components/organism/JobsYouMightLike/style.ts
@@ -0,0 +1,21 @@
+'use client'
+import styled from "styled-components"
+
+
+// -s------ Style Main Jobs You Might Like ------
+export const StyleMainJobsYouMightLike = styled.div`
+
+/* title__tap */
+  .title__tap {
+    display: block;
+    margin: 23px 0 33px;
+    padding-left: 25px;
+
+  }
+  
+  .css-19kzrtu {
+    padding: 0 !important;
+  }
+`;
+// -s------ Style Main Jobs You Might Like ------
+
