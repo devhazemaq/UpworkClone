51bca5b1c90558d2dd66dab8e7d756cc08cd515b
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:59:14 +0300

    Jop information page

---
+++ src/components/pages/JopInformationPage/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import JobDatails from '@/components/organism/JobDatails'
+import React from 'react'
+
+const JopInformationPage = () => {
+  return (
+    <div>
+      <JobDatails />
+    </div>
+  )
+}
+
+export default JopInformationPage
