572a63526578abaa639b1c1a31990ffe3c15f873
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-26 10:03:05 +0300

    Page login

---
+++ src/app/(guest)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "@/components/pages/LoginPage";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  
+};
+
+const Login = () => <LoginPage />;
+
+export default Login;
