6467a945f0d8979325e49d803a8b4a810165f6b2
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-27 21:58:00 +0300

    new constans and contain  placeholder Text Dailog Overview

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -37,3 +37,11 @@ export const paperInformationData: IPaperInfo[] = [
     image: '/assets/images/homeJops/forPaper3.svg',
   }
 ]
+
+
+
+export const  placeHoldTextDailogOverview:string = `Example: I have a Bachelor of Arts (B.A.) in Graphic design from The Higher Education Technical School of Professional Studies in Novi Sad and I am a web and mobile designer with 10+ years of experience.
+
+My range of design services include creating design for new apps (or a new design to improve existing ones), building UI design for mobile, tablet or desktop, making UX designs, wireframes and layout concepts and redesigning websites or adapting to mobile and responsive design. I have worked with many clients such as Replay, O'Neill, Bench, OshKosh &amp; Carters, etc. I'm working professionally with Sketch and Adobe tools.
+
+I will will work with you to discover what you need and create the best solution. My priorities are quality and respect of deadlines and budget. I love to share my enthusiasm and passion for design, so feel free to contact me and let's get the conversation started.`
