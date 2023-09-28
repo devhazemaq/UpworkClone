d9f2f970394622e4a27868143669dc38ab26147f
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-28 23:34:01 +0300

    fix bug the title show tow in above and down

--- src/components/organism/JobsYouMightLike/index.tsx
+++ src/components/organism/JobsYouMightLike/index.tsx
@@ -4,12 +4,12 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import CardJob from '../CardJobs';
 import { SmallText2 } from '@/components/atoms/typography';
 import { StyleMainJobsYouMightLike } from './style';
 import { API_URL_ALLJOPS } from '@/config/api';
 import CardJobs from '../CardJobs';
 import CardsJobsSaved from '../CardsJobsSaved/CardsJobsSaved';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -30,14 +30,14 @@ function CustomTabPanel(props: TabPanelProps) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-          {value === 0 && <CardJobs  />}
+          {value === 0 && <SmallText2 className="title__tap">Browse jobs that match your experience to a client&apos;s hiring preferences. Ordered by most relevant</SmallText2>}
+          {value === 0 && <CardJobs />}
         </Box>
       )}
       {value === index && (
         <Box sx={{ p: 3 }}>
           <Typography>{children}</Typography>
-          {value === 1 && <CardsJobsSaved  />}
+          {value === 1 && <CardsJobsSaved />}
         </Box>
       )}
     </div>
@@ -52,30 +52,37 @@ function a11yProps(index: number) {
 }
 
 export default function BasicTabs() {
+
   const [value, setValue] = React.useState(0);
+  const {saveJobs} = useAppSelector((state)=> state.saveJobs );
+
+  
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
 
+
+
   return (
     <StyleMainJobsYouMightLike>
 
 
-      <Box sx={{ width: '100%' ,border: '1px solid #D5E0D5' }}>
+      <Box sx={{ width: '100%', border: '1px solid #D5E0D5' }}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
             <Tab label="Best Matches" {...a11yProps(0)} />
-            <Tab label="Saved Jobs" {...a11yProps(1)} />
+            {/* <Tab label="Saved Jobs" {...a11yProps(1)} /> */}
+            <Tab label={saveJobs?.length > 0 ? `Saved Jobs(${saveJobs?.length})` : "Saved Jobs" } {...a11yProps(1)} />
             {/* <Tab label="Item Three" {...a11yProps(2)} /> */}
           </Tabs>
         </Box>
         <CustomTabPanel value={value} index={0}>
-          <SmallText2 className="title__tap">Browse jobs that match your experience to a client&apos;s hiring preferences. Ordered by most relevant</SmallText2>
+          {/* <SmallText2 className="title__tap">Browse jobs that match your experience to a client&apos;s hiring preferences. Ordered by most relevant</SmallText2> */}
         </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
-          
+
         </CustomTabPanel>
         {/* <CustomTabPanel value={value} index={2}>
           Item Three
