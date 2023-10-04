1659ab16031c9892d561fc04fd997905192d9913
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-04 13:19:41 +0300

    add profileImg for constants flile and he path imge static

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -45,3 +45,6 @@ export const  placeHoldTextDailogOverview:string = `Example: I have a Bachelor o
 My range of design services include creating design for new apps (or a new design to improve existing ones), building UI design for mobile, tablet or desktop, making UX designs, wireframes and layout concepts and redesigning websites or adapting to mobile and responsive design. I have worked with many clients such as Replay, O'Neill, Bench, OshKosh &amp; Carters, etc. I'm working professionally with Sketch and Adobe tools.
 
 I will will work with you to discover what you need and create the best solution. My priorities are quality and respect of deadlines and budget. I love to share my enthusiasm and passion for design, so feel free to contact me and let's get the conversation started.`
+
+
+export const profileImg:string = "/assets/images/profile/hhh.jpg"
