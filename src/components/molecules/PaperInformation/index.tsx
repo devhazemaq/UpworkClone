24860e71e3f11a25aca6c930f53f07d113fb3cd4
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-04 11:26:03 +0300

    Use corect taqs

--- src/components/molecules/PaperInformation/index.tsx
+++ src/components/molecules/PaperInformation/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { Paper } from '@mui/material'
+import { Box, Paper } from '@mui/material'
 import { Body, SmallText } from '@/components/atoms/typography'
 import Image from 'next/image'
 import { StyleMainPaperInformation } from './style'
@@ -9,37 +9,35 @@ import { paperInformationData } from '@/constants'
 
 
 
-
-
 const PaperInformation: React.FC = () => {
   return (
     <StyleMainPaperInformation>
-      
-      {paperInformationData.map((item)=>(
+
+      {paperInformationData.map((item) => (
         <Paper key={item?.id} elevation={3} className='for__paper' >
-        <div className="contain__info">
-          <Body>{item?.title}</Body>
-          <SmallText>{item?.text1}</SmallText>
-          <SmallText>{item?.text2}</SmallText>
-          <SmallText>{item?.text3}</SmallText>
-        </div>
-        <div className="contain__imgs">
-          <Image
-            src={'/assets/images/homeJops/arrowRight.svg'}
-            alt='arrowRight'
-            width={20}
-            height={20} />
-          <Image
-            src={item?.image}
-            alt='forPaper'
-            width={50}
-            height={40} />
-        </div>
-      </Paper>
-      ))     
+          <Box className="contain__info">
+            <Body>{item?.title}</Body>
+            <SmallText>{item?.text1}</SmallText>
+            <SmallText>{item?.text2}</SmallText>
+            <SmallText>{item?.text3}</SmallText>
+          </Box>
+          <Box className="contain__imgs">
+            <Image
+              src={'/assets/images/homeJops/arrowRight.svg'}
+              alt='arrowRight'
+              width={20}
+              height={20} />
+            <Image
+              src={item?.image}
+              alt='forPaper'
+              width={50}
+              height={40} />
+          </Box>
+        </Paper>
+      ))
       }
 
-      
+
 
     </StyleMainPaperInformation>
   )
