6d02268180ccf289c4c2ed52d593fe7c4b157ebf
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-05 19:34:20 +0300

    gev  NotFound page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h3" color={"green"}>Not Found!!!</Typography>
+    </Box>
+    
+  );
+};
 
-export default NotFound
+export default NotFound;
