9df931812abd89b8fcf0e2a0fba3ceaf63e90e59
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-05 15:21:50 +0300

    add beautiful Loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Image from "next/image";
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <Box 
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', 
+    }}
+    >
+      <Image
+        src="/assets/images/login/logo.svg"
+        alt="logo-upwork"
+        width={120}
+        height={60}
+        loading="eager"
+        priority={true}
+      />
+      
+    </Box>
+  );
+};
 
-export default Loading
+export default Loading;
