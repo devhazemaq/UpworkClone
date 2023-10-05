c285b601d206921abe172d553e74647123231120
devhazemaq <hazemahmedqindeel@gmail.com>
2023-10-05 12:51:49 +0300

    JopInformationPage  get deatils jop from local storage and add title

--- src/components/pages/JopInformationPage/index.tsx
+++ src/components/pages/JopInformationPage/index.tsx
@@ -1,14 +1,36 @@
-'use client'
+"use client";
 
-import JobDatails from '@/components/organism/JobDatails'
-import React from 'react'
+import React from "react";
+import JobDatails from "@/components/organism/JobDatails";
+import { Box, Typography } from "@mui/material";
+import HeaderMain from "@/components/organism/HeaderMain";
+import Container from "@/components/atoms/Container";
 
 const JopInformationPage = () => {
-  return (
-    <div>
-      <JobDatails />
-    </div>
-  )
-}
+  var idJob = null;
 
-export default JopInformationPage
+  if (typeof window !== "undefined") {
+    idJob = localStorage.getItem("jobC");
+  }
+
+  // console.log(idJob);
+
+  let objJob = null;
+
+  if (idJob) {
+    objJob = JSON.parse(idJob);
+  } else {
+    objJob = {};
+  }
+  return (
+    <Box>
+      <HeaderMain />
+      
+        <Typography variant="h5"  pl={"15%"}>Job details</Typography>
+      
+        <JobDatails idhaz={objJob.id} style={{pading:"0 70px"}} />
+    </Box>
+  );
+};
+
+export default JopInformationPage;
